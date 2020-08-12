41024900c00d85a9b9cfc58247a79e30c699225f
georgeo23 <olney.george@yahoo.co.uk>
2020-08-12 17:06:35 +0100

    added keys to each mapped div

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 // import search from "../Page/search";
 
 class Result extends Component {
+   
   render() {
     return (
       <>
-      {this.props.results.map((item, idx) => (
-          <p key={idx}>{item.name}</p>
-      ))}
+      {this.props.results.map((item, idx) =>(
+        <div key = {idx}>
+            <h3>{item.name}</h3>
+            <ul>
+                <li>stargazers:{item.stargazers_count}</li>
+                <li>forks:{item.forks_count}</li>
+                <li>last worked on: {item.updated_at}</li>
+            </ul>
+        </div>
+        )  ) }
       </>
     );
   }
